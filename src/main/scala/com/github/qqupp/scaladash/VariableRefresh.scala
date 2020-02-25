b7f7ad05a8c5fa16c89fbe820a4a61a559cae06b
Paolo Picci <paolo.picci@springernature.com>
2020-02-25 09:44:29 +0000

    wip more sealed traits 1

--- src/main/scala/com/github/qqupp/scaladash/VariableRefresh.scala
+++ src/main/scala/com/github/qqupp/scaladash/VariableRefresh.scala
@@ -1,9 +1,23 @@
 package com.github.qqupp.scaladash
 
+import io.circe.{Encoder, Json}
+
+sealed trait VariableRefresh
+
 object VariableRefresh {
 
-  val Never = 0
-  val OnDashboardLoad = 1
-  val OnTimeRangeChange = 2
+  case object Never extends VariableRefresh
+  case object OnDashboardLoad extends VariableRefresh
+  case object OnTimeRangeChange extends VariableRefresh
+
+  implicit val jsonEncoder: Encoder[VariableRefresh] =
+    variableRefresh =>
+      Json.fromInt(
+        variableRefresh match {
+          case Never => 0
+          case OnDashboardLoad => 1
+          case OnTimeRangeChange => 2
+        }
+      )
 
 }
