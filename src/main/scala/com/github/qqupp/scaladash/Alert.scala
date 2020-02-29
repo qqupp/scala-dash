1a3d58c1e7f792992d0873a1aa44d288e3a44637
Paolo Picci <paolo.picci@springernature.com>
2020-02-29 10:26:53 +0000

    Dashboard test

--- src/main/scala/com/github/qqupp/scaladash/Alert.scala
+++ src/main/scala/com/github/qqupp/scaladash/Alert.scala
@@ -1,44 +1,10 @@
 package com.github.qqupp.scaladash
 
 import com.github.qqupp.scaladash.okish.{ExecutionErrorState, NoDataState, Notification}
+import com.github.qqupp.scaladash.utils.JsonUtils._
 import io.circe.Json
 import io.circe.literal._
-import com.github.qqupp.scaladash.utils.JsonUtils._
 
-/*
-class Alert:
-    def __init__(self, name, frequency, message=None, no_data_state=NoDataState.NoData, execution_error_state=ExecutionErrorState.Alerting):
-        self.name = name
-        self.frequency = frequency
-        self.message = message
-        self.conditions = []
-        self.notifications = []
-        self.no_data_state = no_data_state
-        self.execution_error_state = execution_error_state
-
-    def with_notification(self, notification):
-        self.notifications.append(notification)
-        return self
-
-    def with_condition(self, condition):
-        self.conditions.append(condition)
-        return self
-
-    def build(self, panel_metrics):
-        alert = {
-            "conditions": list(map(lambda condition: condition.build(panel_metrics), self.conditions)),
-            "executionErrorState": self.execution_error_state,
-            "frequency": "%ds" % self.frequency,
-            "handler": 1,
-            "name": self.name,
-            "noDataState": self.no_data_state,
-            "notifications": list(map(lambda notification: notification.build(), self.notifications))
-        }
-        if self.message:
-            alert["message"] = self.message
-        return alert
-
- */
 final case class Alert(name: String,
                        frequencySeconds: Int,
                        message: Option[String],
@@ -47,9 +13,9 @@ final case class Alert(name: String,
                        conditions: List[Condition],
                        notifications: List[Notification]
                       ) {
+
   def withNotification(notification: Notification): Alert =
     copy(notifications = notifications ++ List(notification))
-
 
   def withCondition(condition: Condition): Alert =
     this.copy(conditions = conditions ++ List(condition))
