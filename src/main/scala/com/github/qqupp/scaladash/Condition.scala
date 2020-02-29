d20e3f268246ab079078aa79e1f9a0194f08b5e7
Paolo Picci <paolo.picci@springernature.com>
2020-02-29 10:27:37 +0000

    Dashboard test

--- src/main/scala/com/github/qqupp/scaladash/Condition.scala
+++ src/main/scala/com/github/qqupp/scaladash/Condition.scala
@@ -1,47 +1,9 @@
 package com.github.qqupp.scaladash
 
-import com.github.qqupp.scaladash.okish.{EvaluatorType, Notification, OperatorType, Reducer}
+import com.github.qqupp.scaladash.okish.{EvaluatorType, OperatorType, Reducer}
 import io.circe.Json
 import io.circe.literal._
-/*
 
-class Condition:
-    def __init__(self, metric, evaluator_type, value, operator_type=OperatorType.And, reducer=Reducer.Last, datasource_id=1):
-        self.metric = metric
-        self.evaluator_type = evaluator_type
-        self.value = value
-        self.operator_type = operator_type
-        self.reducer = reducer
-        self.datasource_id = datasource_id
-
-    def build(self, panel_metrics):
-        filtered = filter(lambda possible_metric: possible_metric["target"] == self.metric.target, panel_metrics)
-        matching_metric = list(filtered).pop(0)
-        return {
-            "evaluator": {
-                "params": [self.value],
-                "type": self.evaluator_type
-            },
-            "operator": {
-                "type": self.operator_type
-            },
-            "query": {
-                "datasourceId": self.datasource_id,
-                "model": {
-                    "refId": matching_metric["refId"],
-                    "target": matching_metric["target"]
-                },
-                "params": [matching_metric["refId"], "5m", "now"]
-            },
-            "reducer": {
-                "params": [],
-                "type": self.reducer
-            },
-            "type": "query"
-        }
-
-
- */
 final case class Condition(metric: Metric, evaluatorType: EvaluatorType, value: Int, operatorType: OperatorType, reducer: Reducer, datasourceId: Int) {
 
   def build(toBuildMetrics: List[(String, Metric)]): Json = {
