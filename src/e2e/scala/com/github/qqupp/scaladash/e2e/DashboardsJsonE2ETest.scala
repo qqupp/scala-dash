afa120159c3484214621d49936f7d128f31c83d5
Paolo <paolo.picci@gmail.com>
2020-03-09 20:29:55 +0000

    Add property testing grap panel

--- src/e2e/scala/com/github/qqupp/scaladash/e2e/DashboardsJsonE2ETest.scala
+++ src/e2e/scala/com/github/qqupp/scaladash/e2e/DashboardsJsonE2ETest.scala
@@ -1,8 +1,10 @@
 package com.github.qqupp.scaladash.e2e
 
 import com.github.qqupp.scaladash.e2e.generators.MetricGen
+import com.github.qqupp.scaladash.model.alert.Alert
 import com.github.qqupp.scaladash.model.metric.Metric
-import com.github.qqupp.scaladash.model.panel.GraphPanel
+import com.github.qqupp.scaladash.model.panel._
+import com.github.qqupp.scaladash.model.source.Datasource
 import com.github.qqupp.scaladash.model.{Dashboard, DashboardEnvelope, Row}
 import io.circe.Json
 import org.scalacheck.Arbitrary
@@ -10,8 +12,11 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import sttp.client._
 import sttp.model.StatusCode
+import org.scalacheck.magnolia._
+
 
 class DashboardsJsonE2ETest extends FlatSpec with Matchers with ScalaCheckDrivenPropertyChecks {
+
 
   "A dashboard" should "be json compatible with grafana" in {
 
@@ -25,11 +30,10 @@ class DashboardsJsonE2ETest extends FlatSpec with Matchers with ScalaCheckDriven
 
   }
 
+  implicit lazy val mgen = Arbitrary(MetricGen.allValues)
   "A GraphPanel with Metric" should "be json compatible with grafana" in {
-    implicit val mgen = Arbitrary(MetricGen.allValues)
     forAll{ metric: List[Metric]=>
-      println(metric)
-      val dashboard = Dashboard("GraphPanelRNDTest").withRow(Row().withPanel(GraphPanel("TestPanel").withMetrics(metric)))
+      val dashboard = Dashboard("GraphPanelMetricRNDTest").withRow(Row().withPanel(GraphPanel("TestPanel").withMetrics(metric)))
       val json = DashboardEnvelope.jsonFor(dashboard)
 
       val response = postJsonToLocalGrafana(json)
@@ -37,6 +41,47 @@ class DashboardsJsonE2ETest extends FlatSpec with Matchers with ScalaCheckDriven
       response.code shouldBe StatusCode.Ok
     }
   }
+
+
+  "A GraphPanel" should "be json compatible with grafana" in {
+    final case class TestGraphPanel(title: String,
+                                    metrics: List[Metric],
+                                    yAxisFormat: YAxisFormat,
+                                    filled: FillStyle,
+                                    stacked: StackStyle,
+                                    minimum: YAxisMinimum,
+                                    span: Option[Int],
+                                    maximum: String,
+                                    lines: Boolean,
+                                    bars: Boolean,
+                                    points: Boolean
+    )
+
+
+    forAll{ panel: TestGraphPanel =>
+      val graphPanel =
+        GraphPanel(panel.title)
+          .copy(
+            metrics = panel.metrics,
+            yAxisFormat = panel.yAxisFormat,
+            filled = panel.filled,
+            stacked = panel.stacked,
+            minimum = panel.minimum,
+            span = panel.span,
+            lines = panel.lines,
+            bars = panel.bars,
+            points = panel.points
+          )
+      val dashboard = Dashboard("GraphPanelRNDTest").withRow(Row().withPanel(graphPanel))
+      val json = DashboardEnvelope.jsonFor(dashboard)
+
+      val response = postJsonToLocalGrafana(json)
+
+      response.code shouldBe StatusCode.Ok
+    }
+  }
+
+
 
   private def postJsonToLocalGrafana(json: Json): Identity[Response[Either[String, String]]] = {
     val request =
