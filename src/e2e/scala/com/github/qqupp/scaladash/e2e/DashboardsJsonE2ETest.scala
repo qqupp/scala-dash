82035dbaa75e5df38466adfc5dcdf2d9bf21164b
Paolo <paolo.picci@gmail.com>
2020-03-09 19:42:23 +0000

    Refactoring

--- src/e2e/scala/com/github/qqupp/scaladash/e2e/DashboardsJsonE2ETest.scala
+++ src/e2e/scala/com/github/qqupp/scaladash/e2e/DashboardsJsonE2ETest.scala
@@ -1,6 +1,7 @@
 package com.github.qqupp.scaladash.e2e
 
 import com.github.qqupp.scaladash.model.{Dashboard, DashboardEnvelope}
+import io.circe.Json
 import org.scalatest.{FlatSpec, Matchers}
 import sttp.client._
 import sttp.model.StatusCode
@@ -13,18 +14,20 @@ class DashboardsJsonE2ETest extends FlatSpec with Matchers {
 
     val json = DashboardEnvelope.jsonFor(dashboard)
 
-    val request =
-      basicRequest.post(uri"http://localhost:3000/api/dashboards/db")
-        .header("Content-Type", "application/json")
-        .header("Authorization", "Basic YWRtaW46YWRtaW4=")
-      .body(json.toString)
-
-
-    implicit val backend = HttpURLConnectionBackend()
-    val response = request.send()
+    val response = postJsonToLocalGrafana(json)
 
     response.code shouldBe StatusCode.Ok
 
   }
 
+  private def postJsonToLocalGrafana(json: Json): Identity[Response[Either[String, String]]] = {
+    val request =
+      basicRequest.post(uri"http://localhost:3000/api/dashboards/db")
+        .header("Content-Type", "application/json")
+        .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+        .body(json.toString)
+
+    implicit val backend = HttpURLConnectionBackend()
+    request.send()
+  }
 }
